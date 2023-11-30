b59105a4b9e0bbf7c98299339d40f3eb9c6e8e1f
Kovács Tamás <144217056+kovacs-tamas2004@users.noreply.github.com>
2023-11-30 12:26:47 +0100

    Add files via upload

---
+++ api_kéréssel.js
@@ -0,0 +1,15 @@
+function keres(){
+    const beirMezo = document.getElementById("szoveg").value;
+
+    fetch("https://www.balldontlie.io/api/v1/players?search=" + beirMezo)
+    .then(response => response.json())
+    .then(adat => {
+        for(player of adat.data){
+            document.getElementById("eredmeny").innerHTML += 
+            "Keresztnév: " + player.first_name + " " + "Vezetéknév: " + player.last_name + " " + "Csapata: " + player.team.full_name + "<br>";
+        }
+        //document.getElementById("eredmeny").innerHTML = JSON.stringify(data, null, 2);
+        console.log(data);
+    })
+    .catch(error => console.error("Hiba!" + error))
+}
